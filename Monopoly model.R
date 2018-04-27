755bbf5638f5ec8961089b964d77db62fab2ae50
Natalia Fedorova <nataliafedora23@gmail.com>
2018-04-27 13:37:39 +0200

    Initialization done. lists within lists.

--- Monopoly model.R
+++ Monopoly model.R
@@ -18,21 +18,33 @@ resources <- 10
 
 patches <- list()
 
-patches$patch_ID <- 1:n_patches
+patches$patch_id <- 1:n_patches
 patches$resources <- rep(resources, n_patches)
+patches$bands_id <- list()
 
 
 #create number of bands and band list
 
 bands <- list()
 
-bands$ID <- 1:n_bands
+bands$band_id <- 1:n_bands
 bands$payoff <- rep(0, n_bands)
 bands$fitness <- rep(0, n_bands)
 bands$patch_id <- sample(n_patches, n_bands, replace = TRUE) 
 str(bands)
 
 #need to add bands ID to patches
+index <- list()
+
+
+
+for (i in 1:n_patches){
+    index[[i]] <- which(bands$patch_id == i)
+    if (length(index[[i]]) == 0) {
+      index[[i]] <- "NA"
+    }
+    patches$bands_id[[i]] <- bands$band_id[index[[i]]]
+}
 
 
 
