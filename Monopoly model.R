e530e5e8f412caed76c4e9bd6b398e1b27c76390
Natalia Fedorova <nataliafedora23@gmail.com>
2018-05-03 13:15:55 +0200

    some streamlining, loop for fitness calculation with occupied index, groupsize calculated externally

--- Monopoly model.R
+++ Monopoly model.R
@@ -5,11 +5,13 @@
 
 #it would be better if patches were coordinates pulled from a space - and then summed to get n_patches
 n_patches <- 10
-
 n_bands <- 10
+resources <- 1:10
 
-resources <- 10
-
+#Fitness calculation parameteres
+payoff_default <- 10
+cooperative_benefit <- 0.5
+sigma <- 1
 
 
 #patch list
@@ -19,7 +21,7 @@ resources <- 10
 patches <- list()
 
 patches$patch_id <- 1:n_patches
-patches$resources <- rep(resources, n_patches)
+patches$resources <- sample(resources, n_patches, replace = TRUE)
 patches$bands_id <- list()
 
 
@@ -35,14 +37,38 @@ str(bands)
 
 #need to add bands ID to patches
 
+#take some time to look over this
 for (i in 1:n_patches){
   temp <- which(bands$patch_id == i)
   if (length(temp) == 0) {
-    patches$bands_id[[i]] <- "NA"
+    patches$bands_id[[i]] <- NA
   } else {
     patches$bands_id[[i]] <- bands$band_id[temp]
   }
 }
+
+
+#fitness calculation
+
+#occupied index so that calculations are not done on irrelevant patches
+index_occupied <- which(!is.na(patches$bands_id))
+
+#groupsize calculation 
+#do I need to track groupsize separate from the fitness calculation? because otherwise I can just put this into the payoff calculation
+for (i in 1: length(index_occupied)){
+    bands$groupsize[index_occupied[i]] <- length(patches$bands_id[[index_occupied[i]]])
+
+      #payoff calculation
+      bands$payoff[index_occupied[i]] <- rnorm( 1 , mean = payoff_default + (bands$groupsize[index_occupied[i]] - 1)^cooperative_benefit, sigma)
+      #the 1 in rnorm is number of observations
+      
+          #fitness calculation 
+          #directly calculating group foraging payoff and fitness 
+          if((bands$payoff[i]*bands$groupsize[i]) > patches$resources[bands$patch_id[i]]   )
+  
+}
+
+
 
 
 
