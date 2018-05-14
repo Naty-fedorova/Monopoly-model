11a94c7cd2f5c406db83912719489fe61e69225a
Natalia Fedorova <nataliafedora23@gmail.com>
2018-05-14 14:13:25 +0200

    Fixes to birth and death process - death goes first, -c() notation for deletes, new ids for added bands

--- Monopoly model.R
+++ Monopoly model.R
@@ -83,80 +83,64 @@ for (i in 1: length(bands$band_id)){
         bands$fitness[i] <- patches$resources[bands$patch_id[i]]/bands$group_size[i]
       } else {
         bands$fitness[i] <- patches$payoff[bands$patch_id[i]]/bands$group_size[i]
-      }
-}
-
-
-
-###BIRTH AND DEATH PROCESS###
-
-for (i in 1:length(bands$band_id)){
-
-      #Birth
+      } 
         
-      #Get probability of birth for each band  
-      #FIX ME - for now, storing birth/death prob in bands, but probably don't need to store it (make temp vector)  
-      bands$birth_prob[i] <- rep_rate*(bands$fitness[i]/bands$payoff[i])
-      
-      #Get birth/not birth of each band
-      #FIX ME - same here, doesn't need to be stored, just temp vector
-      bands$birth[i] <- rbinom(1, size = 1, prob = bands$birth_prob[i])
-      
-      #Death
-      
-      #Get probability of death for each band
-      #FIX ME - doesn't need to be stored
-      bands$death_prob[i] <- 1/(1+exp(1)^((death_par_1*bands$fitness[i])-death_par_2))
-
-      #Get death/not death of each band
-      #FIX ME - doesn't need to be stored
-      bands$death[i] <- rbinom(1, size = 1, prob = bands$death_prob[i])
-      
+            ###BIRTH AND DEATH PROCESS###
+            #FIX ME - for now, storing birth/death prob in bands, but probably don't need to store it (make temp vector) 
+            
+            #Birth
+            
+            #Get probability of birth for each band  
+            bands$birth_prob[i] <- rep_rate*(bands$fitness[i]/bands$payoff[i])
+            
+            #Get birth/not birth of each band
+            bands$birth[i] <- rbinom(1, size = 1, prob = bands$birth_prob[i])
+            
+            #Death
+            
+            #Get probability of death for each band
+            bands$death_prob[i] <- 1/(1+exp(1)^((death_par_1*bands$fitness[i])-death_par_2))
+            
+            #Get death/not death of each band
+            bands$death[i] <- rbinom(1, size = 1, prob = bands$death_prob[i])
+        
+        
 }
 
-#FIX ME - think about how to do it in one go, instead of adding births and then subtracting deaths
-#this is actually quite a big problem because when they are already added, they will not be removed correctly by the deaths
-#but by doing the below loop, we as if always kill off new borns - so if we do not clone them then it matters - DISCUSS
-
-#An attempt at above
-for (i in 1: length(bands$band_id)){
-  #if both of those are true
-  if((as.logical(bands$birth[i]))&(as.logical(bands$death[i])) == TRUE){
-    #then they both have to become 0 i.e. they cancel each other out
-    bands$birth[i] <- 0
-    bands$death[i] <- 0
-    #otherwise both stay the same 
-  } 
-}
-
-#After the above for loop, I should only have unique birth and death events (not those that cancel each other out)
+#Index of which bands died
+death_index <- which(bands$death == 1)
 
 
-#Remove dead bands
-#FIX ME - doesn't work because index repeats once its length finishes - fixed for now by putting death process first
-bands$band_id[as.logical(bands$death)] <- NA 
-bands$payoff[as.logical(bands$death)] <- NA 
-bands$fitness[as.logical(bands$death)] <- NA 
-bands$patch_id[as.logical(bands$death)] <- NA 
-bands$group_size[as.logical(bands$death)] <- NA
+#Remove dead from bands and birth index
+#FIX ME - should I also remove these from the death and death_prob vectors? 
+bands$band_id <- bands$band_id[-(death_index)]  
+bands$payoff <- bands$payoff[-(death_index)]
+bands$fitness <- bands$fitness[-(death_index)]  
+bands$patch_id <- bands$patch_id[-(death_index)]
+bands$group_size <- bands$group_size[-(death_index)]
+#FIX ME - birth_prob won't be necessary once it isn't being stored, just now so it doesn't look confusing 
+bands$birth_prob <- bands$birth_prob[-(death_index)]
+bands$birth <- bands$birth[-(death_index)]
+
+#Index of bands that are reproducing
+birth_index <- which(bands$birth == 1)
 
 
-#Clone band_id, payoff, fitness, patch_id
-#FIX ME - for now, new bands are clones of old ones, but need to think about whether this works
-#FIX ME - at the moment id is cloned which will cause problems further upstream - need unique ids for new agents
-bands$band_id <- append(bands$band_id, bands$band_id[as.logical(bands$birth)], after = length(bands$band_id)) 
-bands$payoff <- append(bands$payoff, bands$payoff[as.logical(bands$birth)], after = length(bands$band_id))
-bands$fitness <- append(bands$fitness, bands$fitness[as.logical(bands$birth)], after = length(bands$band_id))
-bands$patch_id <- append(bands$patch_id, bands$patch_id[as.logical(bands$birth)], after = length(bands$band_id))
+#Clone band_id, payoff, fitness, patch_id of those bands that are giving birth, assign new band_id though
+#generate new ids
+new_ids <- rep((n_bands+1):(n_bands+length(birth_index)))
+
+#Appends new ids to band_id, and clone value of reproduced bands for payoff, fitness, and patch_id
+bands$band_id <- append(bands$band_id, new_ids, after = length(bands$band_id)) 
+bands$payoff <- append(bands$payoff, bands$payoff[birth_index])
+bands$fitness <- append(bands$fitness, bands$fitness[birth_index])
+bands$patch_id <- append(bands$patch_id, bands$patch_id[birth_index])
+
+#Not updating groupsize because that needs to be recalculated
 
 
-
-#NOTE - group size is not updated as of yet
 
 #TO DO
-#discuss what to do about cloning, and therefore whether to have loop getting unique birth/death
-#recalculate group size after birth/death process
-#discuss and code not storing birth and death probabilities and vectors
 #update patches list based on births and deaths
 
 
