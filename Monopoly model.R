e7ab6007a195bff25c765d40b024d134b1daf6fb
Natalia Fedorova <nataliafedora23@gmail.com>
2018-05-17 14:37:36 +0200

    Detailed comments

--- Monopoly model.R
+++ Monopoly model.R
@@ -55,10 +55,12 @@ for (j in 1:timesteps){
         
       #Add bands ID to patches list
       for (i in 1:n_patches){
+        #Temporary vector that stores index of which patch_id is the same as i
         temp <- which(bands$patch_id == i)
         if (length(temp) == 0) {
           patches$bands_id[[i]] <- NA
         } else {
+          #If the temp vector actually includes something, it is used to index band_id and put it into the patches list
           patches$bands_id[[i]] <- bands$band_id[temp]
         }
       }
@@ -68,27 +70,20 @@ for (j in 1:timesteps){
       
       #Groupsize and payoff calculation 
       for(i in 1:length(bands$band_id)){
-            
-            # calculate groupsizes for each agent  
+            # calculate groupsizes for each agent by indexing band id's in patches by patch ids in bands and getting the length of that (since its a vector, kinda)  
             bands$group_size[i] <- length(patches$bands_id[[bands$patch_id[i]]])
-            
-            #calculate each bands payoff 
+            #calculate each bands payoff using crema's equation and sampling from a normal distribution, the 1 in rnorm is number of observations
             bands$payoff[i] <- rnorm(1, mean = payoff_default + (bands$group_size[i] -1)^cooperative_benefit, sigma)
-            #the 1 in rnorm is number of observations
       }
         
 
       #Calculate patch payoff by summing payoff of bands in that patch 
       for (i in 1: n_patches){
-          
             if(length(patches$bands_id) > 0){
-            
+            #If a patch has some bands, we store the index of those bands in a temp vector 
             temp_2 <- match(unlist(patches$bands_id[i]), bands$band_id)
-            
+            #Use the temp vector to index payoff in bands and get the sum of all the payoffs of the bands in that patch
             patches$payoff[i] <- sum(bands$payoff[temp_2]) 
-            
-            #here i need what to do with occupied patches
-            
             } else {
               # if that patch is not occupied, put in an na for payoff
               patches$payoff[i] <- NA
@@ -98,9 +93,11 @@ for (j in 1:timesteps){
 
       #Calculate fitness based on density dependence
       for (i in 1: length(bands$band_id)){
+            #If patch payoff is more than patch resources, band fitness is resources/groupsize
             if(patches$payoff[bands$patch_id[i]] >= patches$resources[bands$patch_id[i]]){
               bands$fitness[i] <- patches$resources[bands$patch_id[i]]/bands$group_size[i]
             } else {
+              #Otherwise, band fitness is patch payoff/groupsize
               bands$fitness[i] <- patches$payoff[bands$patch_id[i]]/bands$group_size[i]
             } 
               
@@ -109,28 +106,22 @@ for (j in 1:timesteps){
                   
                   #Birth
                   
-                  #Get probability of birth for each band  
+                  #Get probability of birth for each band, crema's function  
                   bands$birth_prob[i] <- rep_rate*(bands$fitness[i]/bands$payoff[i])
-                  
-                  #Get birth/not birth of each band
+                  #Get birth/not birth of each band, bernoulli draw
                   bands$birth[i] <- rbinom(1, size = 1, prob = bands$birth_prob[i])
                   
                   #Death
-                  
-                  #Get probability of death for each band
+                  #Get probability of death for each band, crema's function
                   bands$death_prob[i] <- 1/(1+exp(1)^((death_par_1*bands$fitness[i])-death_par_2))
-                  
-                  #Get death/not death of each band
+                  #Get death/not death of each band, bernoulli draw
                   bands$death[i] <- rbinom(1, size = 1, prob = bands$death_prob[i])
-              
-              
       }
       
       #Index of which bands died
       death_index <- which(bands$death == 1)
       
-      
-      #Remove dead from bands and birth index, if death index is more than 0
+      #Remove dead from bands and birth index, if death index is more than 0, using c-() notation and death index
       if(length(death_index) > 0){
       bands$band_id <- bands$band_id[-(death_index)]  
       bands$payoff <- bands$payoff[-(death_index)]
@@ -144,17 +135,16 @@ for (j in 1:timesteps){
       bands$death <- bands$death[-(death_index)]
       } 
       
-      
       #Index of bands that are reproducing
       birth_index <- which(bands$birth == 1)
       
       #Generate new id's, n = length of birth_index, i.e. n of new bands, using max id for this
       if(length(birth_index) > 0){
+        #Repetitions where n=the lenght of the birth index, and the ids go from the max id +1 to the max id + length of birth index
         new_ids <- rep((max(bands$band_id)+1):(max(bands$band_id)+length(birth_index)))
       } else {
         new_ids <- NULL
       }
-      
       
       #Appends new ids to band_id, and clone value of reproduced bands for payoff, fitness, and patch_id
       bands$band_id <- append(bands$band_id, new_ids, after = length(bands$band_id)) 
