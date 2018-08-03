5ff49cebf3ef1e16fbfa2615d4bc026dcff3c207
Natalia Fedorova <nataliafedora23@gmail.com>
2018-08-03 11:03:48 +0200

    working version of groupsize function

--- groupsize function.R
+++ groupsize function.R
@@ -2,7 +2,7 @@
 
 calc_groupsize <- function(bands, patches){
   
-  #this is a function (type: method) which calculate groupsizes for each agent by indexing band id's in patches by patch ids in bands and getting the length
+  #this is a function which calculate groupsizes for each agent by indexing band id's in patches by patch ids in bands and getting the length
   
   #arguments: band list, patch list
   
@@ -11,8 +11,9 @@ calc_groupsize <- function(bands, patches){
   groupsizes <- rep(0, length(bands$patch_id))
   
   for(i in 1:length(bands$patch_id)){
-    groupsizes<- length(patches$bands_id[[bands$patch_id[i]]])
+    groupsizes[i]<- length(patches$bands_id[[bands$patch_id[i]]])
   }
   return(groupsizes)
 }
 
+#bands$groupsize <- calc_groupsize(bands = bands, patches = patches)
