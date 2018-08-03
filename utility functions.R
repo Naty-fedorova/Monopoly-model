773368c84526b3dba242362e03901e8c5d49c7a6
Natalia Fedorova <nataliafedora23@gmail.com>
2018-08-03 11:03:55 +0200

    renaming

---
+++ utility functions.R
@@ -0,0 +1,18 @@
+#Groupsize function
+
+calc_groupsize <- function(bands, patches){
+  
+  #this is a function which calculate groupsizes for each agent by indexing band id's in patches by patch ids in bands and getting the length
+  
+  #arguments: band list, patch list
+  
+  #return - vector of groupsizes
+  
+  groupsizes <- rep(0, length(bands$band_id))
+  
+  for(i in 1:length(bands$band_id)){
+    groupsizes[i]<- length(patches$bands_id[[bands$patch_id[i]]])
+  }
+  return(groupsizes)
+}
+
