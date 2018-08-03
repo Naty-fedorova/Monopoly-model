378076b6d7377a3754309c79e798631df294b318
Natalia Fedorova <nataliafedora23@gmail.com>
2018-08-03 11:24:53 +0200

    working version of bands_in_patches function

--- utility functions.R
+++ utility functions.R
@@ -1,3 +1,35 @@
+
+#Adding band ids to patch list
+
+bands_in_patches <- function(bands, patches, n_patches){
+  
+  #This is a function which takes band ids from the band list and matches them with the correct patch id indice in patches list
+  
+  #argument: band list, patch list, n_patches
+  
+  #return: patch_band list of lists
+  
+  #initialize list of lists
+  
+  ids_in_patches <- list()
+  
+  #Add bands ID to patches list
+  for (i in 1:n_patches){
+    #Temporary vector that stores index of which patch_id is the same as i
+    temp <- which(bands$patch_id == i)
+    if (length(temp) == 0) {
+      ids_in_patches[[i]] <- NA
+    } else {
+      #If the temp vector actually includes something, it is used to index band_id and put it into the patches list
+      ids_in_patches[[i]]<- bands$band_id[temp]
+    }
+  }
+  
+  return(ids_in_patches)
+  
+}
+
+
 #Groupsize function
 
 calc_groupsize <- function(bands, patches){
